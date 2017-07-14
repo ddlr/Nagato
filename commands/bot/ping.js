6ecc655ebe036a1221388115dcccab556ff6340f
ddlr <epic.emolga@gmail.com>
2017-07-14 20:23:59 +1200

    Change ping.js to follow npm code guide or w/e

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -1,20 +1,23 @@
 // thx zeyla
-BaseCommand = require('../../lib/Abstracts/BaseCommand');
+
+'use strict'
+
+const BaseCommand = require('../../lib/Abstracts/BaseCommand')
 
 class Ping extends BaseCommand {
   constructor(bot, type) {
-    super(bot, type);
+    super(bot, type)
 
-    this.aliases = ['p'];
+    this.aliases = ['p']
   }
 
   get name() {
-    return 'ping';
+    return 'ping'
   }
 
   async process(msg) {
-    await this.execute(msg, 'Pong!');
+    await this.execute(msg, 'Pong!')
   }
 }
 
-module.exports = Ping;
+module.exports = Ping
